6e26b0d9efe136761e0ac1c7b3d55e6eb8e8ff5b
Christopher Michael Buck <butterwell@gmail.com>
2014-07-31 11:58:19 -0400

    klee-cc full path

--- klee-coreutil.sh
+++ klee-coreutil.sh
@@ -4,6 +4,9 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 
+sudo apt-get install python-pip
+sudo pip install tabulate
+
 cd ~/source
 curl -O http://ftp.gnu.org/gnu/coreutils/coreutils-6.11.tar.gz
 tar xvzf coreutils-6.11.tar.gz
@@ -15,7 +18,18 @@ cd obj-gcov
 ../configure --disable-nls CFLAGS="-g -fprofile-arcs -ftest-coverage"
 make
 make -C src arch hostname
-cd src
+
+# llvm
+cd ~/source/coreutils-6.11
+mkdir obj-llvm
+cd obj-llvm
+../configure --disable-nls CFLAGS="-g"
+export cc=~/source/klee/scripts/klee-gcc
+make CC=$cc
+make -C src arch hostname CC=$cc
+
+# visual tests
+cd ~/source/coreutils-6.11/src/obj-gcov
 ls -l ls echo cat
 ./cat --version
 rm -f *.gcda
@@ -23,14 +37,7 @@ rm -f *.gcda
 ls -l echo.gcda
 gcov echo
 
-# llvm
-cd ~/source/coreutils-6.11
-mkdir obj-llvm
-cd obj-llvm
-../configure --disable-nls CFLAGS="-g"
-make CC=../../klee/scripts/klee-gcc
-make -C src arch hostname CC=../../klee/scripts/klee-gcc
-cd src
+cd ~/source/coreutils-6.11/src/obj-llvm
 ls -l ls echo cat
 klee --libc=uclibc --posix-runtime ./cat.bc --version
 klee --libc=uclibc --posix-runtime ./echo.bc --help
@@ -38,5 +45,4 @@ klee --libc=uclibc --posix-runtime ./echo.bc --sym-arg 3
 klee-stats klee-last
 klee --optimize --libc=uclibc --posix-runtime ./echo.bc --sym-arg 3
 klee-stats klee-last
-
 
