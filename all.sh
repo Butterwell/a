0ccf29656defdf63b9fc3abccbcfe85e75183da5
Christopher Michael Buck <butterwell@gmail.com>
2014-07-31 09:27:02 -0400

    lseek64 fix

--- all.sh
+++ all.sh
@@ -23,8 +23,8 @@ sudo apt-get install -y curl python-minimal git bison flex bc libcap-dev cmake b
 
 # All source in source, tools in tools
 cd ~
-mkdir source
-mkdir tools
+[ ! -e source ] && mkdir source
+[ ! -e tools ] && mkdir tools
 
 # cryptominisat 4 (first class sat used by stp)
 #  could build after: git clone https://github.com/msoos/cryptominisat.git
@@ -48,30 +48,58 @@ echo 'PATH=~/tools/llvm-gcc4.2-2.9-x86_64/bin:$PATH' >> ~/.bashrc
 echo 'export PATH' >> ~/.bashrc
 export PATH=~/tools/llvm-gcc4.2-2.9-x86_64/bin:$PATH
 
-#  llvm 2.9 release (to be built by llvm-gcc compiler, use to build klee)
+#  llvm 2.9 release (build in source)
 cd ~/source
 [ ! -e llvm-2.9.tgz ] && curl -O http://llvm.org/releases/2.9/llvm-2.9.tgz
 [ ! -e llvm-2.9 ] && tar xvzf llvm-2.9.tgz
+# Fix lseek64 not found, from: http://www.mail-archive.com/klee-dev@imperial.ac.uk/msg01302.html
+patch << EOF
+diff -u -r llvm-2.9/lib/ExecutionEngine/JIT/Intercept.cpp src/lib/ExecutionEngine/JIT/Intercept.cpp
+--- llvm-2.9/lib/ExecutionEngine/JIT/Intercept.cpp	2010-11-29 18:16:10.000000000 +0000
++++ src/lib/ExecutionEngine/JIT/Intercept.cpp	2013-09-27 12:11:02.464085889 +0100
+@@ -50,6 +50,7 @@
+ #if defined(__linux__)
+ #if defined(HAVE_SYS_STAT_H)
+ #include <sys/stat.h>
++#include <unistd.h>
+ #endif
+ #include <fcntl.h>
+ /* stat functions are redirecting to __xstat with a version number.  On x86-64 
+EOF
 cd llvm-2.9
-# cmake? 
-#./configure --enable-optimized --enable-assertions
-#make
+[ ! -e Release+Asserts ] && ./configure --enable-optimized --enable-assertions
+make
 
-# STP building
-#cd ~/tools
-#git clone https://github.com/stp/stp.git
-#cd stp
+# There is cmake materials in llvm-2.9. If they work, this is what you'd do:
 #mkdir build
 #cd build
 #cmake ..
 #make
-#sudo make install
 
-#echo 'ulimit -s unlimited' >> ~/.bashrc
-#ulimit -s unlimited
+# STP building (build in tools)
+cd ~/source
+[ ! -e stp ] && git clone https://github.com/stp/stp.git
+cd ~/tools
+[ ! -e stp ] && mkdir stp
+cd stp
+cmake ~/source/stp
+make
+sudo make install
 
+echo 'ulimit -s unlimited' >> ~/.bashrc
+ulimit -s unlimited
 
-# klee-uclibc building
-#cd ~/tools
-#git clone --depth 1 --branch klee_0_9_29 https://github.com/ccadar/klee-uclibc.git
+# klee-uclibc building (build in source)
+cd ~/source
+[ ! -e klee-uclibc ] && git clone --depth 1 --branch klee_0_9_29 https://github.com/ccadar/klee-uclibc.git
+cd klee-uclibc
+./configure --make-llvm-lib
+make
 
+# KLEE
+cd ~/source
+[ ! -e klee ] && git clone git clone https://github.com/klee/klee.git
+cd klee
+./configure --with-llvm=~/source/llvm-2.9 --with-stp=~/tools/stp --with-uclibc=~/source/klee-uclibc --enable-posix-runtime
+
+echo "Restart terminal for new PATH (new tools)"
